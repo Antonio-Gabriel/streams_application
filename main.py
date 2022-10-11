0cd962c469080aa1c06b46f14d47fbf41b1c3edd
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-10-11 09:06:47 +0100

    feat: add data to bulk data file

---
+++ main.py
@@ -0,0 +1,16 @@
+import io
+import json
+
+from mock import faker, transactions_faker
+
+with io.open("bulkData.file", "w+", buffering=20*(1024**2)) as file:    
+    for i in range(1, int(1e3)): 
+        user = dict(
+            id=int(i),
+            name=str(faker.name()),
+            email=str(faker.email()),
+            address=str(faker.address()),
+            transactions=transactions_faker
+        )
+
+        file.writelines(json.dumps(user))
